a28dc2b72c8e332c6abadb989dd2a99be9f3c496
Hariprasath390 <harihariprasath106@gmail.com>
2023-11-29 11:57:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/url', { originalUrl });
+      setShortUrl(response.data.shortUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const displayShortenedUrl = () => {
+    const fullShortUrl = `http://localhost:5000/${shortUrl}`;
+
+    return (
+      <div>
+        <p>Shortened URL:</p>
+        <a href={fullShortUrl} target="_blank" rel="noopener noreferrer">
+          {fullShortUrl}
+        </a>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>URL Shortener</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Original URL:
+          <input
+            type="url"
+            value={originalUrl}
+            onChange={(e) => setOriginalUrl(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Shorten URL</button>
+      </form>
+      {shortUrl && displayShortenedUrl()}
     </div>
   );
 }
